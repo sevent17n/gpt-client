c3a9312754f5d835e9a56538b4485436f61f8d73
NikitaSdev <nikitashadowrazev@gmail.com>
2023-04-25 10:07:13 +0300

    authorization implemented 0.1 no username provided

--- app/Auth/Auth.tsx
+++ app/Auth/Auth.tsx
@@ -47,7 +47,7 @@ export default function Auth() {
               <input type="submit" />
             </form>
             <LoginButton
-              botUsername={"https://t.me/Djipiti_test_bot"}
+              botUsername={"Djipiti_test_bot"}
               buttonSize="large" // "large" | "medium" | "small"
               cornerRadius={5} // 0 - 20
               showAvatar={true} // true | false
