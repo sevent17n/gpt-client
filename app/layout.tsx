690debdf96bf9158d6695fe353f4755f2779824d
NikitaSdev <nikitashadowrazev@gmail.com>
2023-07-03 23:49:17 +0300

    metrika added

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,26 @@ export default function RootLayout({
           rel="stylesheet"
         ></link>
         <script src="/serviceWorkerRegister.js" defer></script>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      
+                ym(94194196, "init", {
+                      clickmap:true,
+                      trackLinks:true,
+                      accurateTrackBounce:true
+                });
+              `,
+            }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/94194196" style={{ position:'absolute', left:'-9999px' }} alt="" />
+          </div>
+        </noscript>
       </head>
       <body>{children}</body>
     </html>
