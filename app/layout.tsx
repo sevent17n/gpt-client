1bc001ddaf290937fa2197fbec43be462925b216
Nikita <107374098+NikitaSdev@users.noreply.github.com>
2023-08-06 10:28:55 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,49 @@ export default function RootLayout({
         />
 
         <Script src="/serviceWorkerRegister.js" defer></Script>
-       
+       <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-NSWE0W06R3"
+        />
+        <Script
+          id={"google"}
+          dangerouslySetInnerHTML={{
+            __html: `
+                 (window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-NSWE0W06R3');
+              `,
+          }}
+        />
+        <Script
+          id={"yandex"}
+          dangerouslySetInnerHTML={{
+            __html: `
+                 (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(994543374, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+              `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/994543374"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </head>
       <body>
  <Script
@@ -100,7 +142,7 @@ export default function RootLayout({
    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(93246004, "init", {
+   ym(994543374, "init", {
         clickmap:true,
         trackLinks:true,
         accurateTrackBounce:true,
@@ -112,7 +154,7 @@ export default function RootLayout({
         <noscript>
           <div>
             <img
-              src="https://mc.yandex.ru/watch/93246004"
+              src="https://mc.yandex.ru/watch/994543374"
               style={{ position: "absolute", left: "-9999px" }}
               alt=""
             />
